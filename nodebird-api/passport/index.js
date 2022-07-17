af7faea6371d0f4eafb91be19926500cf23cc61c
hyukjinKimm <hyukjin.kim.dev@gmail.com>
2022-07-17 15:29:24 +0900

    first commit

---
+++ nodebird-api/passport/index.js
@@ -0,0 +1,35 @@
+const passport = require('passport');
+const local = require('./localStrategy');
+const kakao = require('./kakaoStrategy');
+const User = require('../models/user');
+
+module.exports = () => {
+  passport.serializeUser((user, done) => {
+    done(null, user.id);
+   
+  });
+ // 세션에 user 의 id만 저장
+ // { id: 3, 'connect.sid': s%32349384 }
+
+
+  //세션쿠키 안의 있는 sid 에 해당되는 id 가 인수로 들어감
+  passport.deserializeUser((id, done) => {
+    User.findOne({
+      where: { id },
+      include: [{
+        model: User,
+        attributes: ['id', 'nick'],
+        as: 'Followers',
+      }, {
+        model: User,
+        attributes: ['id', 'nick'],
+        as: 'Followings',
+      }],
+    })
+      .then(user => done(null, user))
+      .catch(err => done(err));
+  });
+
+  local();
+  kakao();
+};
